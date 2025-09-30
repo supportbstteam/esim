87b58d716d262fd3c62a17fa3a76a90616846315
zuber57 <Zubarahmad48@gmail.com>
2025-09-30 19:04:24 +0530

    Added the design

--- src/components/modals/AuthModal.tsx
+++ src/components/modals/AuthModal.tsx
@@ -74,149 +74,175 @@ const AuthModal: React.FC<AuthModalProps> = ({ isOpen, onClose }) => {
 
     return (
         <div
-            className="fixed inset-0 bg-black bg-opacity-0 flex justify-center items-center z-50"
+            className="fixed inset-0 bg-[#00000073] backdrop-blur-sm bg-opacity-0 flex justify-center items-center z-50 "
             onClick={onClose}
         >
-            <div
-                className="max-w-md w-full p-8 bg-white rounded-lg shadow-lg"
-                onClick={(e) => e.stopPropagation()}
-            >
-                {/* Toggle Buttons */}
-                <div className="flex justify-center mb-6 space-x-4">
-                    <button
-                        className={`px-6 py-2 rounded-md font-semibold ${isLogin ? "bg-[#133365] text-white shadow-md" : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-                            }`}
-                        onClick={() => setIsLogin(true)}
-                        disabled={isLogin}
-                    >
-                        Login
-                    </button>
-                    <button
-                        className={`px-6 py-2 rounded-md font-semibold ${!isLogin ? "bg-[#133365] text-white shadow-md" : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-                            }`}
-                        onClick={() => setIsLogin(false)}
-                        disabled={!isLogin}
-                    >
-                        Sign Up
-                    </button>
+            <div className="flex bg-white w-full px-4 md:px-0  md:!w-[835px] md:h-[630px]">
+                <div className="w-[40%] py-14 px-[25px] bg-[#D0DFF4]">
+                    <h2 className="h1">Your Gateway to Smarter Connectivity</h2>
+                    <p className="mt-9 subtext">
+                        Log in or sign up to explore affordable, secure, and borderless mobile experiences.
+                    </p>
                 </div>
+                <div
+                    className="w-[60%] px-14 py-5 bg-white"
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    {/* Toggle Buttons */}
+                     <h2 className="h2 text-start">
+                     {isLogin ? "Welcome Back" : "Create Your Account"}
 
-                {/* ======= Form ======= */}
-                {isLogin ? (
-                    <Formik
-                        initialValues={loginInitialValues}
-                        validationSchema={loginValidationSchema}
-                        onSubmit={handleLoginSubmit}
-                    >
-                        <Form className="space-y-6">
-                            {/* Email */}
-                            <div>
-                                <label htmlFor="email" className="block mb-1 font-medium text-gray-700">
-                                    Email
-                                </label>
-                                <Field
-                                    name="email"
-                                    type="email"
-                                    autoComplete="username"
-                                    className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                                <ErrorMessage name="email" component="div" className="mt-1 text-red-600 text-sm" />
-                            </div>
+                     </h2>
+                    <div className="flex justify-center mb-6  mt-6 bg-[#F3F5F7] rounded-2xl ">
+                        <button
+                            className={`px-6 py-2 w-full rounded-2xl font-semibold ${!isLogin ? "bg-[#3BC85221] text-[#3BC852] " : "bg-[#F3F5F7] text-gray-700 "
+                                }`}
+                            onClick={() => setIsLogin(false)}
+                            disabled={!isLogin}
+                        >
+                            Register
+                        </button>
+                        <button
+                            className={`px-6 py-2 w-full rounded-2xl  font-semibold ${isLogin ? "bg-[#3BC85221] text-[#3BC852] " : "bg-[#F3F5F7] text-gray-700 "
+                                }`}
+                            onClick={() => setIsLogin(true)}
+                            disabled={isLogin}
+                        >
+                            Login
+                        </button>
+                    </div>
 
-                            {/* Password */}
-                            <div>
-                                <label htmlFor="password" className="block mb-1 font-medium text-gray-700">
-                                    Password
-                                </label>
-                                <Field
-                                    name="password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                                <ErrorMessage name="password" component="div" className="mt-1 text-red-600 text-sm" />
-                            </div>
+                    {/* ======= Form ======= */}
+                    {isLogin ? (
+                        <Formik
+                            initialValues={loginInitialValues}
+                            validationSchema={loginValidationSchema}
+                            onSubmit={handleLoginSubmit}
+                        >
+                            <Form className="space-y-6">
+                                {/* Email */}
+                                <div>
+                                    <label htmlFor="email" className="block mb-1 font-medium text-gray-700">
+                                        Email
+                                    </label>
+                                    <Field
+                                        name="email"
+                                        type="email"
+                                        autoComplete="username"
+                                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    />
+                                    <ErrorMessage name="email" component="div" className="mt-1 text-red-600 text-sm" />
+                                </div>
 
-                            <button
-                                type="submit"
-                                className="w-full py-3 bg-[#133365] text-white font-semibold rounded-md hover:bg-blue-700 transition"
-                            >
-                                Login
-                            </button>
-                        </Form>
-                    </Formik>
-                ) : (
-                    <Formik
-                        initialValues={signupInitialValues}
-                        validationSchema={signupValidationSchema}
-                        onSubmit={handleSignupSubmit}
-                    >
-                        <Form className="space-y-6">
-                            {/* First Name */}
-                            <div>
-                                <label htmlFor="firstName" className="block mb-1 font-medium text-gray-700">
-                                    First Name
-                                </label>
-                                <Field
-                                    name="firstName"
-                                    type="text"
-                                    autoComplete="given-name"
-                                    className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                                <ErrorMessage name="firstName" component="div" className="mt-1 text-red-600 text-sm" />
-                            </div>
+                                {/* Password */}
+                                <div>
+                                    <label htmlFor="password" className="block mb-1 font-medium text-gray-700">
+                                        Password
+                                    </label>
+                                    <Field
+                                        name="password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    />
+                                    <ErrorMessage name="password" component="div" className="mt-1 text-red-600 text-sm" />
+                                </div>
 
-                            {/* Last Name */}
-                            <div>
-                                <label htmlFor="lastName" className="block mb-1 font-medium text-gray-700">
-                                    Last Name
-                                </label>
-                                <Field
-                                    name="lastName"
-                                    type="text"
-                                    autoComplete="family-name"
-                                    className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                                <ErrorMessage name="lastName" component="div" className="mt-1 text-red-600 text-sm" />
-                            </div>
+                                <button
+                                    type="submit"
+                                    className="w-full py-3 bg-[#3BC852] text-white font-semibold rounded-md  transition"
+                                >
+                                    Submit
+                                </button>
+                            </Form>
+                        </Formik>
+                    ) : (
+                        <Formik
+                            initialValues={signupInitialValues}
+                            validationSchema={signupValidationSchema}
+                            onSubmit={handleSignupSubmit}
+                        >
+                            <Form className="space-y-6">
+                                {/* First Name */}
+                                <div className="flex gap-3">
+                                    <div className="">
+                                        <label htmlFor="firstName" className="block mb-1 font-medium text-gray-700">
+                                            First Name
+                                        </label>
+                                        <Field
+                                            name="firstName"
+                                            type="text"
+                                            autoComplete="given-name"
+                                            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        />
+                                        <ErrorMessage name="firstName" component="div" className="mt-1 text-red-600 text-sm" />
+                                    </div>
 
-                            {/* Email */}
-                            <div>
-                                <label htmlFor="email" className="block mb-1 font-medium text-gray-700">
-                                    Email
-                                </label>
-                                <Field
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                                <ErrorMessage name="email" component="div" className="mt-1 text-red-600 text-sm" />
-                            </div>
+                                    {/* Last Name */}
+                                    <div className="">
+                                        <label htmlFor="lastName" className="block mb-1 font-medium text-gray-700">
+                                            Last Name
+                                        </label>
+                                        <Field
+                                            name="lastName"
+                                            type="text"
+                                            autoComplete="family-name"
+                                            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        />
+                                        <ErrorMessage name="lastName" component="div" className="mt-1 text-red-600 text-sm" />
+                                    </div>
+                                </div>
+                                {/* Email */}
+                                <div>
+                                    <label htmlFor="email" className="block mb-1 font-medium text-gray-700">
+                                        Email
+                                    </label>
+                                    <Field
+                                        name="email"
+                                        type="email"
+                                        autoComplete="email"
+                                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    />
+                                    <ErrorMessage name="email" component="div" className="mt-1 text-red-600 text-sm" />
+                                </div>
 
-                            {/* Password */}
-                            <div>
-                                <label htmlFor="password" className="block mb-1 font-medium text-gray-700">
-                                    Password
-                                </label>
-                                <Field
-                                    name="password"
-                                    type="password"
-                                    autoComplete="new-password"
-                                    className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                                <ErrorMessage name="password" component="div" className="mt-1 text-red-600 text-sm" />
-                            </div>
+                                {/* Password */}
+                                <div>
+                                    <label htmlFor="password" className="block mb-1 font-medium text-gray-700">
+                                        Password
+                                    </label>
+                                    <Field
+                                        name="password"
+                                        type="password"
+                                        autoComplete="new-password"
+                                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    />
+                                    <ErrorMessage name="password" component="div" className="mt-1 text-red-600 text-sm" />
+                                </div>
 
-                            <button
-                                type="submit"
-                                className="w-full py-3 bg-[#133365] text-white font-semibold rounded-md hover:bg-blue-700 transition"
-                            >
-                                Sign Up
-                            </button>
-                        </Form>
-                    </Formik>
-                )}
+                                <div>
+                                    <label htmlFor="password" className="block mb-1 font-medium text-gray-700">
+                                        Referral Coupon
+                                    </label>
+                                    <Field
+                                        name="password"
+                                        type="password"
+                                        autoComplete="new-password"
+                                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    />
+                                    <ErrorMessage name="password" component="div" className="mt-1 text-red-600 text-sm" />
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    className="w-full py-3 bg-[#3BC852] text-white font-semibold rounded-md hover:bg-blue-700 transition"
+                                >
+                                    Submit
+                                </button>
+                            </Form>
+                        </Formik>
+                    )}
+                </div>
             </div>
         </div>
     );
