7443824411f433fb2680aa30caad62476749cbae
bstteam120 <bstteam120@gmail.com>
2025-10-13 17:23:33 +0530

    new up

--- src/components/home/TravelPlans.tsx
+++ src/components/home/TravelPlans.tsx
@@ -18,39 +18,40 @@ const TravelPlans = () => {
 
 
       {/* Cards */}
-     <div
-  className={`mt-12 flex flex-wrap gap-4 sm:gap-6 ${
-    countries.length === 1 ? "justify-center" : "justify-start"
-  }`}
->
-  {countries.map((item: any, i: number) => (
-    <div
-      key={i}
-      onClick={() => handleNavigate(item.id)}
-      className="border border-gray-200 rounded-[8px] hover:bg-green-50 hover:border-green-500 transition duration-300 cursor-pointer w-full sm:w-[48%] lg:w-[31%] xl:w-[23%]"
-    >
-      <div className="p-6 flex items-center justify-between gap-4">
-        {/* Flag + Name */}
-        <div className="flex items-center gap-4">
-          <Flag
-            countryName={item.name}
-            size={36}
-            className="h-[36px] w-[36px]"
-          />
-          <p className="text-sm sm:text-base md:text-lg font-medium">
-            {item.name}
-          </p>
-        </div>
+      <div className="mt-12 grid gap-4 sm:gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          countries.map((item: any, i: number) => {
+            // console.log("---- item ----", item);
+            return (
+              <div
+                key={i}
+                onClick={() => handleNavigate(item.id)}
+                className="border border-gray-200 rounded-[8px] hover:bg-green-50 hover:border-green-500 transition duration-300 cursor-pointer"
+              >
+                <div className="p-6 flex items-center justify-between gap-4"
+                >
+                  {/* Flag + Name */}
+                  <div className="flex items-center gap-4">
+                    <Flag
+                      countryName={item.name}
+                      size={36}
+                      className="h-[36px] w-[36px]"
+                    />
+                    <p className="text-sm sm:text-base md:text-lg font-medium">
+                      {item.name}
+                    </p>
+                  </div>
 
-        {/* Arrow */}
-        <div className="text-xl sm:text-2xl text-gray-500">
-          <IoIosArrowForward />
-        </div>
+                  {/* Arrow */}
+                  <div className="text-xl sm:text-2xl text-gray-500">
+                    <IoIosArrowForward />
+                  </div>
+                </div>
+              </div>
+            )
+          })}
       </div>
-    </div>
-  ))}
-</div>
-
 
       {/* Explore Button */}
       <div className="flex justify-center mt-12 text-gray-600 hover:text-green-600 transition">
