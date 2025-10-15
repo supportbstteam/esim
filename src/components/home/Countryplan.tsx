7651d4014a8d4a2c8c83e0eb545ce3fe85d37520
bstteam120 <bstteam120@gmail.com>
2025-10-15 17:59:27 +0530

    Featured: About us page designed

--- src/components/home/Countryplan.tsx
+++ src/components/home/Countryplan.tsx
@@ -82,7 +82,7 @@ export default function CountryplanTabs() {
               aria-selected={active === 'country'}
               role="tab"
               onClick={() => setActive('country')}
-              className={`relative px-4 md:px-10 py-2 text-[16px] border-1 rounded-full border-[#ffffff] md:text-[21px] text-white font-medium transition-all focus:outline-none ${active === 'country'
+              className={`relative   px-3 max-[340px]:px-1 md:px-10  py-2 text-[14px] border-1 rounded-full border-[#ffffff] md:text-[21px] text-white font-medium transition-all focus:outline-none ${active === 'country'
                 ? "!bg-[#3BC852] !border-[#3BC852] after:content-[''] after:absolute after:h-[10px] after:w-[10px] after:bg-[#3bc952] after:bottom-[-10px] after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rotate-[223deg]"
                 : 'hover:'
                 }`}
@@ -94,7 +94,7 @@ export default function CountryplanTabs() {
               aria-selected={active === 'popular'}
               role="tab"
               onClick={() => setActive('popular')}
-              className={`relative px-4 md:px-10 py-2 text-[16px] border-1 rounded-full border-[#ffffff] md:text-[21px] text-white font-medium transition-all focus:outline-none ${active === 'popular'
+              className={`relative  px-3 max-[340px]:px-1 md:px-10  py-2 text-[14px] border-1 rounded-full border-[#ffffff]  md:text-[21px] text-white font-medium transition-all focus:outline-none ${active === 'popular'
                 ? "!bg-[#3BC852] !border-[#3BC852] after:content-[''] after:absolute after:h-[10px] after:w-[10px] after:bg-[#3bc952] after:bottom-[-10px] after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rotate-[223deg]"
                 : 'hover:'
                 }`}
