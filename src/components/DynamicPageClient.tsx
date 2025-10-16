dcd47b8c941b26d199bd2fd71225a4ea676b3b0d
bstteam120 <bstteam120@gmail.com>
2025-10-16 11:28:35 +0530

    featured term and condition and privacy policy page

--- src/components/DynamicPageClient.tsx
+++ src/components/DynamicPageClient.tsx
@@ -4,7 +4,9 @@ import { api } from "@/lib/api";
 import { fetchUserDetails } from "@/redux/slice/UserSlice";
 import { useAppDispatch } from "@/redux/store";
 import React, { useEffect, useState } from "react";
-
+import TrustedTravel from '@/components/home/TrustedTravel';
+import FAQ from "@/components/home/Faq";
+import { useAppSelector } from "@/redux/store";
 interface Props {
     page: string;
 }
@@ -12,6 +14,7 @@ interface Props {
 export default function DynamicPageClient({ page }: Props) {
     const [content, setContent] = useState<string>("");
     const [loading, setLoading] = useState(false);
+    const { list } = useAppSelector((state) => state?.faq);
     const [error, setError] = useState<string | null>(null);
     const dispatch = useAppDispatch();
     const title = page
@@ -44,27 +47,37 @@ export default function DynamicPageClient({ page }: Props) {
     }, [page]);
 
     return (
-        <div className="p-4 container">
-            <h1 className="h1 font-bold mb-4 mt-5 md:mt-12">{title}</h1>
+        <div>
+            <div className="p-4 container">
+                <h1 className="h1 font-bold mb-4 mt-5 md:mt-12">{title}</h1>
 
-            {loading && (
-                <div className="space-y-3 animate-pulse">
-                    <div className="h-6 w-3/4 bg-gray-300 rounded"></div>
-                    <div className="h-4 w-full bg-gray-300 rounded"></div>
-                    <div className="h-4 w-full bg-gray-300 rounded"></div>
-                    <div className="h-4 w-5/6 bg-gray-300 rounded"></div>
-                    <div className="h-4 w-2/3 bg-gray-300 rounded"></div>
-                </div>
-            )}
+                {loading && (
+                    <div className="space-y-3 animate-pulse">
+                        <div className="h-6 w-3/4 bg-gray-300 rounded"></div>
+                        <div className="h-4 w-full bg-gray-300 rounded"></div>
+                        <div className="h-4 w-full bg-gray-300 rounded"></div>
+                        <div className="h-4 w-5/6 bg-gray-300 rounded"></div>
+                        <div className="h-4 w-2/3 bg-gray-300 rounded"></div>
+                    </div>
+                )}
 
-            {error && <p className="subtext">{error}</p>}
+                {error && <p className="subtext">{error}</p>}
 
-            {!loading && !error && (
-                <div
-                    className="prose max-w-none"
-                    dangerouslySetInnerHTML={{ __html: content }}
-                />
-            )}
+                {!loading && !error && (
+                    <div
+                        className="prose max-w-none"
+                        dangerouslySetInnerHTML={{ __html: content }}
+                    />
+                )}
+            </div>
+            {title === 'Terms And Conditions' || 'Privacy Policy' 
+            ?
+            (<div>
+                 <TrustedTravel />
+        <FAQ faqs={list} />
+            </div>): (<div></div>)
+             }
+
         </div>
     );
 }
