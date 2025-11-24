0bb08eeec2c5749abfe1df4f50ec06af9908e6df
bstteam102 <bstteam102@gmail.com>
2025-11-24 10:50:11 +0530

    work on tailwind issues

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx,mdx}",
-    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
-    "./components/**/*.{js,jsx,ts,tsx,mdx}",
-    "./src/**/*.{js,jsx,ts,tsx,mdx}",
-  ],
-}
