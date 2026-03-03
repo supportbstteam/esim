a2493edadfc635cc260aa047cd1d81f3b230f14a
supportbstteam <support@betasofttechnology.com>
2026-03-03 15:04:20 +0530

    reafctor: made email non editable in the website

--- src/components/modals/EditProfileModal.tsx
+++ src/components/modals/EditProfileModal.tsx
@@ -134,6 +134,7 @@ export const EditProfileModal: React.FC<Props> = ({
                 <Field
                   name="email"
                   type="email"
+                  disabled
                   className="w-full border rounded-md px-3 py-2 text-sm focus:ring-2 focus:ring-blue-200 outline-none"
                   />
                 <ErrorMessage name="email" component="div" className="text-xs text-rose-500 mt-1" />
